9752ac970bc11c3bc6044f7c81644eb4910ab3ce
Your Name <hello@kevincadena.com>
2023-03-26 21:05:39 -0400

    archive

---
+++ ndxzsite/config/config.php
@@ -0,0 +1,9 @@
+<?php  if (!defined('SITE')) exit('No direct script access allowed');
+
+$indx['db'] 		= 'indexexhibit';
+$indx['user'] 		= 'kcadena';
+$indx['pass'] 		= 'Fuckyou66!';
+$indx['host'] 		= 'sushitimecocom.ipagemysql.com';
+$indx['sql']		= 'mysql';
+
+if (!defined('PX')) { define('PX', 'ndxzbt_'); }
